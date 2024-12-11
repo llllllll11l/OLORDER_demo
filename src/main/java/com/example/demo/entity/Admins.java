7bcae709d4cd4cf41369689eb46d964e0636718d
XGCstar <1071781763@qq.com>
2024-12-11 20:36:42 +0800

    test

--- src/main/java/com/example/demo/entity/Admins.java
+++ src/main/java/com/example/demo/entity/Admins.java
@@ -1,5 +1,11 @@
 package com.example.demo.entity;
 
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Timestamp;
+@Data
 public class Admins {
 
 }
